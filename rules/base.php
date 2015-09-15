e0f408cc3674524ea480f80e8a67d41a655d151f
Julian Calaby <julian.calaby@gmail.com>
2015-09-15 10:18:49 +1000

    Update existing rules to better rule syntax

--- rules/base.php
+++ rules/base.php
@@ -1,10 +1,14 @@
 <?php
 // SQL queries
-FAIL == mysql_query(UNSAFE);
+FAIL(SAFE == mysql_query(UNSAFE));
 
 // HTML leakage
-FAIL == print(UNSAFE);
-echo UNSAFE == FAIL;
+FAIL(SAFE == print(UNSAFE));
+echo FAIL(SAFE == UNSAFE); // Weird syntax due to echo not being a function
+
+// File access
+FAIL(UNSAFE == fopen(UNSAFE));
+FAIL(UNSAFE == file_get_contents(UNSAFE));
 
 // Places where we can get user data
 UNSAFE == $_REQUEST;
@@ -13,50 +17,46 @@ UNSAFE == $_POST;
 UNSAFE == $_COOKIE;
 UNSAFE == $_SERVER;
 UNSAFE == $_FILES;
-UNSAFE == file_get_contents(SAFE);
+UNSAFE == file_get_contents(NULL);
 
 // HTML / URL quoting
-AUDIT == htmlspecialchars(UNSAFE);
-AUDIT == htmlentities(UNSAFE);
-AUDIT == urlencode(UNSAFE);
+AUDIT(SAFE == htmlspecialchars(UNSAFE));
+AUDIT(SAFE == htmlentities(UNSAFE));
+AUDIT(SAFE == urlencode(UNSAFE));
 
 // Other quoting
-AUDIT == addslashes(UNSAFE);
-AUDIT == preg_quote(UNSAFE);
-AUDIT == quotemeta(UNSAFE);
-AUDIT == escapeshellarg(UNSAFE);
-AUDIT == escapeshellcmd(UNSAFE);
-AUDIT == ldap_escape(UNSAFE);
-AUDIT == curl_escape(UNSAFE);
+AUDIT(SAFE == addslashes(UNSAFE));
+AUDIT(SAFE == preg_quote(UNSAFE));
+AUDIT(SAFE == quotemeta(UNSAFE));
+AUDIT(SAFE == escapeshellarg(UNSAFE));
+AUDIT(SAFE == escapeshellcmd(UNSAFE));
+AUDIT(SAFE == ldap_escape(UNSAFE));
+AUDIT(SAFE == curl_escape(UNSAFE));
 
 // Database quoting
-AUDIT == pg_escape_bytea(UNSAFE);
-AUDIT == pg_escape_identifier(UNSAFE);
-AUDIT == pg_escape_string(UNSAFE);
-AUDIT == pg_escape_literal(UNSAFE);
-AUDIT == db2_escape_string(UNSAFE);
-AUDIT == dbx_escape_string(UNSAFE);
-AUDIT == mysql_escape_string(UNSAFE);
-AUDIT == maxdb_escape_string(UNSAFE);
-AUDIT == maxdb::real_escape_string(UNSAFE);
-AUDIT == mysqli_escape_string(UNSAFE);
-AUDIT == mysqli_real_escape_string(UNSAFE);
-AUDIT == ingres_escape_string(UNSAFE);
-AUDIT == sqlite_escape_string(UNSAFE);
-AUDIT == maxdb_real_escape_string(UNSAFE);
-AUDIT == mysql_real_escape_string(UNSAFE);
-AUDIT == cubrid_real_escape_string(UNSAFE);
-AUDIT == PDO::quote(UNSAFE);
+AUDIT(SAFE == pg_escape_bytea(UNSAFE));
+AUDIT(SAFE == pg_escape_identifier(UNSAFE));
+AUDIT(SAFE == pg_escape_string(UNSAFE));
+AUDIT(SAFE == pg_escape_literal(UNSAFE));
+AUDIT(SAFE == db2_escape_string(UNSAFE));
+AUDIT(SAFE == dbx_escape_string(UNSAFE));
+AUDIT(SAFE == mysql_escape_string(UNSAFE));
+AUDIT(SAFE == maxdb_escape_string(UNSAFE));
+AUDIT(SAFE == maxdb::real_escape_string(UNSAFE));
+AUDIT(SAFE == mysqli_escape_string(UNSAFE));
+AUDIT(SAFE == mysqli_real_escape_string(UNSAFE));
+AUDIT(SAFE == ingres_escape_string(UNSAFE));
+AUDIT(SAFE == sqlite_escape_string(UNSAFE));
+AUDIT(SAFE == maxdb_real_escape_string(UNSAFE));
+AUDIT(SAFE == mysql_real_escape_string(UNSAFE));
+AUDIT(SAFE == cubrid_real_escape_string(UNSAFE));
+AUDIT(SAFE == PDO::quote(UNSAFE));
 
 // Compression
-AUDIT == gzencode(UNSAFE);
-AUDIT == gzdeflate(UNSAFE);
-AUDIT == gzcompress(UNSAFE);
+AUDIT(SAFE == gzencode(UNSAFE));
+AUDIT(SAFE == gzdeflate(UNSAFE));
+AUDIT(SAFE == gzcompress(UNSAFE));
 
 // Hashing
 SAFE == md5(UNSAFE);
 SAFE == sha1(UNSAFE);
-
-// Primitives
-SAFE == "";
-SAFE == 1;
